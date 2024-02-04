bcb5771a3ec06404773326d7312ed55a78a75a32
alex-delia <adelia3@uwo.ca>
2024-02-04 02:42:26 -0500

    create operate function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -5,3 +9,15 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+function operate(num1, num2, operator) {
+    if (operator === '+') {
+        add(num1, num2);
+    } else if (operator === '-') {
+        subtract(num1, num2);
+    } else if (operator === '*') {
+        multiply(num1, num2);
+    } else if (operator === '/') {
+        divide(num1, num2);
+    }
+}
