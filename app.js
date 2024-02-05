0556947b6754bf9acfa2eded563c710ca0fa726d
alex-delia <adelia3@uwo.ca>
2024-02-04 23:19:35 -0500

    add full calculator logic and styling

--- app.js
+++ app.js
@@ -1,23 +1,141 @@
-let num1;
-let num2;
-let operator;
+let num1 = null;
+let num2 = null;
+let operator = null;
+let displayText = '0';
+let currentSelect = 'num';
 
-const add = (a, b) => a + b;
+let display = document.querySelector('.display');
+let buttons = document.querySelectorAll('button');
 
-const subtract = (a, b) => a - b;
 
-const multiply = (a, b) => a * b;
+buttons.forEach(function (button) {
+    button.addEventListener('click', printDisplay);
+});
 
-const divide = (a, b) => a / b;
+
+function printDisplay() {
+    if (this.classList.contains('num')) {
+        if (currentSelect !== 'num' || displayText === '0') {
+            if (currentSelect === 'equals' || displayText === 'NO NO NO') {
+                num1 = null;
+                num2 = null;
+                operator = null;
+            }
+            displayText = this.textContent;
+        } else {
+            if (displayText.length < 8) {
+                console.log("TEST");
+                displayText += this.textContent;
+            }
+        }
+        display.textContent = displayText;
+        currentSelect = 'num';
+    } else if (this.classList.contains('operator')) {
+        if (currentSelect === 'operator' || currentSelect === 'equals') {
+            operator = this.textContent;
+        } else {
+            if (num1 === null) {
+                num1 = parseFloat(displayText);
+                // console.log('Num1: ' + num1);
+                operator = this.textContent;
+            } else {
+                num2 = parseFloat(displayText);
+                // console.log('Num2: ' + num2);
+                displayText = operate(num1, num2, operator);
+                display.textContent = displayText;
+                num1 = displayText;
+                operator = this.textContent;
+                num2 = null;
+            }
+        }
+        currentSelect = 'operator';
+    } else if (this.classList.contains('equals')) {
+        if (num1 !== null && operator) {
+            num2 = parseFloat(displayText);
+            // console.log('Num2: ' + num2);
+            displayText = operate(num1, num2, operator);
+            display.textContent = displayText;
+            num1 = parseFloat(displayText);
+            // console.log('Num1: ' + num1);
+            operator = null;
+            num2 = null;
+            currentSelect = 'equals';
+        }
+    } else if (this.classList.contains('clear')) {
+        displayText = '0';
+        display.textContent = displayText;
+        num1 = null;
+        num2 = null;
+        operator = null;
+        currentSelect = 'clear';
+        buttons.forEach(function (button) {
+            button.addEventListener('click', printDisplay);
+        });
+    } else if (this.classList.contains('dot')) {
+        if (displayText.indexOf('.') === -1) {
+            displayText += this.textContent;
+            display.textContent = displayText;
+        }
+    }
+    console.log('Current Select: ' + currentSelect);
+}
+
+const add = (a, b) => {
+    let value = a + b;
+    if (value > 99999999) {
+        value = value.toExponential(2);
+        console.log('Value: ' + value);
+    }
+    return value;
+};
+
+const subtract = (a, b) => {
+    let value = a - b;
+    if (value > 99999999) {
+        value = value.toExponential(2);
+        console.log('Value: ' + value);
+    }
+    return value;
+}
+
+const multiply = (a, b) => {
+    let value = a * b;
+    if (value > 99999999) {
+        value = value.toExponential(2);
+        console.log('Value: ' + value);
+    }
+
+    if (value % 1 !== 0) {
+        value = Math.round(value * 100) / 100;
+    }
+
+    return value;
+}
+
+const divide = (a, b) => {
+    let value = a / b;
+    if (value > 99999999) {
+        value = value.toExponential(2);
+        console.log('Value: ' + value);
+    }
+    if (value % 1 !== 0) {
+        value = Math.round(value * 100) / 100;
+    }
+
+    return value;
+}
 
 function operate(num1, num2, operator) {
     if (operator === '+') {
-        add(num1, num2);
+        return add(num1, num2);
     } else if (operator === '-') {
-        subtract(num1, num2);
+        return subtract(num1, num2);
     } else if (operator === '*') {
-        multiply(num1, num2);
+        return multiply(num1, num2);
     } else if (operator === '/') {
-        divide(num1, num2);
+        if (num2 === 0) {
+            return 'NO NO NO'
+        }
+        return divide(num1, num2);
     }
 }
